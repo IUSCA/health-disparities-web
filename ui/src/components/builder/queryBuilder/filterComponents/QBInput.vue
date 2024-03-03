0d6162c730f2b0021b1d1cc6fcade53f8b5c0455
Deepak Duggirala <ddeepak6992@gmail.com>
2024-03-03 09:45:29 -0500

    Fix QBInput.vue styles

--- ui/src/components/builder/queryBuilder/filterComponents/QBInput.vue
+++ ui/src/components/builder/queryBuilder/filterComponents/QBInput.vue
@@ -5,9 +5,7 @@
 <script setup></script>
 
 <style scoped lang="scss">
-:deep(.cohort-builder-input) {
-  .va-input-wrapper__field {
-    --va-input-wrapper-min-height: 20px;
-  }
+:deep(.va-input-wrapper__field) {
+  --va-input-wrapper-min-height: 20px;
 }
 </style>
