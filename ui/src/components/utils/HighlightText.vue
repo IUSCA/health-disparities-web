6cd45e81393be00352ea780d86fa75f696085ca2
Deepak Duggirala <ddeepak6992@gmail.com>
2024-07-16 10:21:10 -0400

    feat: Add HighlightText component for highlighting keywords in content

---
+++ ui/src/components/utils/HighlightText.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-html="highlightedContent"></div>
+</template>
+
+<script setup>
+const props = defineProps({
+  content: String,
+  keyword: String,
+});
+
+const highlightedContent = computed(() => {
+  const regex = new RegExp(`(${props.keyword})`, "gi");
+  return props.content.replace(
+    regex,
+    '<span style="background-color: var(--va-text-highlighted);">$1</span>',
+  );
+});
+</script>
+
+<style scoped>
+/* Additional styles if needed */
+</style>
