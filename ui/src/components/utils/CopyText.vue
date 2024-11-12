d00fdaf1cd92bfcb5b2566b1d6f1aa028d481b1e
Deepak Duggirala <ddeepak6992@gmail.com>
2024-11-12 00:35:30 -0500

    Enhance CopyText and CopyButton components with size prop and improved styling

--- ui/src/components/utils/CopyText.vue
+++ ui/src/components/utils/CopyText.vue
@@ -1,7 +1,16 @@
 <template>
-  <va-input type="text" readonly :model-value="props.text" class="flex-auto">
+  <va-input
+    type="text"
+    readonly
+    :model-value="props.text"
+    class="flex-auto"
+    :class="
+      props.size === 'small' ? 'text-sm copy-text-input-small' : 'text-base'
+    "
+    preset="bordered"
+  >
     <template #appendInner>
-      <CopyButton :text="props.text" preset="plain" />
+      <CopyButton :text="props.text" preset="plain" :size="props.size" />
     </template>
   </va-input>
 </template>
@@ -9,5 +18,12 @@
 <script setup>
 const props = defineProps({
   text: String,
+  size: String,
 });
 </script>
+
+<style scoped lang="scss">
+.copy-text-input-small :deep(.va-input-wrapper__field) {
+  --va-input-wrapper-min-height: 24px;
+}
+</style>
--- ui/src/components/utils/buttons/CopyButton.vue
+++ ui/src/components/utils/buttons/CopyButton.vue
@@ -2,25 +2,30 @@
   <div v-if="isSupported" class="flex items-center justify-center">
     <va-popover
       :message="copied ? 'Copied' : 'Copy'"
-      :hover-over-timeout="1000"
+      :hover-over-timeout="500"
       class="flex-none"
+      placement="right"
     >
-      <va-button
-        @click="
-          () => {
-            copy(props.text);
-            // Emit an event to allow parent to react to the copy-text action
-            $emit('textCopied');
-          }
-        "
-        :preset="props.preset"
-      >
-        <i-mdi-check-bold style="color: var(--va-success)" v-if="copied" />
+      <va-button @click="handleCopy" :preset="props.preset" :size="props.size">
+        <i-mdi-check-bold
+          v-if="copied"
+          style="color: var(--va-success)"
+          key="check"
+          class="icon"
+        />
         <i-mdi-content-copy
           v-else
           style="color: var(--va-primary)"
-          class="hover:brightness-200"
+          class="hover:brightness-200 icon"
+          key="copy"
         />
+      </va-button>
+    </va-popover>
+  </div>
+  <div v-else>
+    <va-popover message="Copy is not supported in this browser">
+      <va-button :preset="props.preset" disabled>
+        <i-mdi-content-copy style="color: var(--va-disabled)" />
       </va-button>
     </va-popover>
   </div>
@@ -30,14 +35,28 @@
 import { useClipboard } from "@vueuse/core";
 
 const props = defineProps({
-  text: String,
+  text: {
+    type: String,
+    required: true,
+  },
   preset: {
     type: String,
     default: "primary",
   },
+  size: {
+    type: String,
+    default: "small",
+  },
 });
+const emit = defineEmits(["textCopied"]);
 
 const { copy, copied, isSupported } = useClipboard({
   copiedDuring: 3000,
 });
+
+function handleCopy() {
+  copy(props.text);
+  // Emit an event to allow parent to react to the copy-text action
+  emit("textCopied");
+}
 </script>
