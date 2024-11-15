23425e38a48dd1aebeb61cb44b5e9ab844e8871a
Deepak Duggirala <ddeepak6992@gmail.com>
2024-11-15 13:04:25 -0500

    Update client generation process and add example environment configuration

--- api/generateClient.sh
+++ api/generateClient.sh
@@ -2,5 +2,5 @@ docker run --rm \
     -v $PWD:/local openapitools/openapi-generator-cli generate \
     -i /local/swagger_public.json \
     -g bash \
-    -o /local/out/bash \
+    -o /local/clients/bash \
     -c /local/clientGenerationConfig.json
