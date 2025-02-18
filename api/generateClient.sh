59072c454c275e1723abfc7e11dd5589bec42624
Deepak Duggirala <ddeepak6992@gmail.com>
2025-02-18 00:14:22 -0500

    Enhance generateClient.sh with comments for clarity on parameters and usage

--- api/generateClient.sh
+++ api/generateClient.sh
@@ -1,6 +1,12 @@
 docker run --rm \
-    -v $PWD:/local openapitools/openapi-generator-cli generate \
-    -i /local/swagger_public.json \
-    -g bash \
-    -o /local/clients/bash \
-    -c /local/clientGenerationConfig.json
+    -v $PWD:/local \
+    openapitools/openapi-generator-cli generate \
+      -i /local/swagger_public.json \
+      -g bash \
+      -o /local/clients/bash \
+      -c /local/clientGenerationConfig.json
+
+# -i: input OpenAPI specification file
+# -g: generator type
+# -o: the output directory for the generated code
+# -c: a configuration file for customizing code generation
