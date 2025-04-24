bb48b8a791c0916e837e69becedc4714dfec8b2f
Deepak Duggirala <ddeepak6992@gmail.com>
2025-04-24 14:00:18 -0400

    add schema migrations for access requests

---
+++ api/prisma/migrations/20250424174500_request_status/migration.sql
@@ -0,0 +1,13 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "RequestStatus" ADD VALUE 'INITIATED';
+ALTER TYPE "RequestStatus" ADD VALUE 'APPROVED_WITH_REVISIONS';
+ALTER TYPE "RequestStatus" ADD VALUE 'CANCELED';
+ALTER TYPE "RequestStatus" ADD VALUE 'EXPIRED';
+
