5f3fbeca0ae8bb27c3872aed8137805be5b87dbe
deepakduggirala <deepakduggirala@users.noreply.github.com>
2025-04-24 14:09:40 -0400

    Stage direct (#107)
    * employ batching in stage direct
    
    * refactor main function to accept file path as command line argument
    
    * fix logging and improve line filtering in stage_direct script
    
    * refactor main function to use for-of loop for batch processing
    
    * enhance error handling in trigger_wf function to differentiate assertion errors

--- api/src/scripts/stage_direct.js
+++ api/src/scripts/stage_direct.js
@@ -1,43 +1,65 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
 /* eslint-disable no-console */
 const fsPromises = require('fs/promises');
+// const { setTimeout } = require('timers/promises');
+
 const { PrismaClient } = require('@prisma/client');
+const _ = require('lodash/fp');
+
 const datasetService = require('../services/dataset');
 
 const prisma = new PrismaClient();
+const BATCH_SIZE = 10;
 
 async function trigger_wf(name, dataset_type) {
-  const ds = await prisma.dataset.findFirst({
-    where: {
-      name,
-      type: dataset_type,
-      is_staged: false,
-    },
-  });
+  try {
+    const ds = await prisma.dataset.findFirst({
+      where: {
+        name,
+        type: dataset_type,
+        is_staged: false,
+      },
+    });
 
-  if (!ds) {
-    console.log(`skipped staging ${name}`);
-    return;
+    if (!ds) {
+      console.log(`skipped staging ${name}`);
+      return;
+    }
+
+    const dataset = await datasetService.get_dataset({
+      id: ds.id,
+      workflows: true,
+    });
+
+    const wf = await datasetService.create_workflow(dataset, 'stage_direct');
+    console.log(`Created workflow ${wf.workflow_id} for dataset ${ds.name} (${ds.id})`);
+  } catch (e) {
+    if (e.name === 'AssertionError') {
+      console.error(`Assertion error processing ${name}: ${e.message}`);
+    } else {
+      console.error(`Error processing ${name}:`, e);
+    }
   }
-
-  const dataset = await datasetService.get_dataset({
-    id: ds.id,
-    workflows: true,
-  });
-
-  const wf = await datasetService.create_workflow(dataset, 'stage_direct');
-  console.log(`Created workflow ${wf.id} for dataset ${ds.id}`);
 }
 
 async function main() {
-  const txt_file_path = '/opt/sca/app/list_of_broadID_to_release_for_Sean_McCabe.txt';
+  const txt_file_path = process.argv[2];
+  if (!txt_file_path) {
+    console.error('Error: Please provide the path to the text file as a command line argument.');
+    process.exit(1);
+  }
   const txt = await fsPromises.readFile(txt_file_path, 'utf8');
-  const lines = txt.split('\n');
+  const lines = txt.split('\n').filter((line) => line.trim() !== '');
   // console.log(lines)
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const name of lines) {
-    // eslint-disable-next-line no-await-in-loop
-    await trigger_wf(name, 'DATA_PRODUCT');
+  let num_processed = 0;
+  const batches = _.chunk(BATCH_SIZE)(lines);
+  for (const batch of batches) {
+    await Promise.all(batch.map((name) => trigger_wf(name, 'DATA_PRODUCT')));
+    // await setTimeout(1000);
+    num_processed += batch.length;
+    console.log(`Processed: ${num_processed}`);
   }
 }
 
@@ -45,8 +67,8 @@ async function main() {
 //   return trigger_wf('SM-NACU2', 'DATA_PRODUCT');
 // }
 
-main().then((result) => {
-  console.log(result);
+main().then(() => {
+  // console.log(result);
   prisma.$disconnect();
 }).catch((e) => {
   console.error(e);
